bbdb7968c61fd74bc657730254173adaa8e8558c
Michael van Niekerk <mvanniekerk@jhb.dvt.co.za>
2019-05-31 10:00:53 +0200

    CSS fixed prod build

---
+++ precache-manifest.3e35354d0a064f9afd4546e91041ff21.js
@@ -0,0 +1,88 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa6df0bd13c002481c98db1e202ec8a",
+    "url": "content/images/frame.svg"
+  },
+  {
+    "url": "content/main.d14b65e1607afe4dd6c8.css"
+  },
+  {
+    "url": "content/vendors.d14b65e1607afe4dd6c8.css"
+  },
+  {
+    "revision": "def92d7607e76b376dd23d21a2ba6551",
+    "url": "index.html"
+  },
+  {
+    "revision": "a67b8d690ca9b440a338ef9e34194b93",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "d4a5d598e8510d03a187a3a2b8bda2e0",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "be66612e582d0f05047a7cf3e3ddc606",
+    "url": "manifest.webapp"
+  },
+  {
+    "revision": "d001004f765a00ec6cd7f89c782e506a",
+    "url": "content/images/spoon-ui256.png"
+  },
+  {
+    "revision": "72fd8c159e274e016b28ac1b46a34f02",
+    "url": "content/images/triangle.svg"
+  },
+  {
+    "revision": "b8bec99789f3b935c26c36ac3658ca18",
+    "url": "content/images/spoon-ui384.png"
+  },
+  {
+    "url": "content/72fd8c159e274e016b28ac1b46a34f02.svg"
+  },
+  {
+    "revision": "b77cd177df9e44a1d3eb06e1cafce30d",
+    "url": "content/images/spoon-ui512.png"
+  },
+  {
+    "revision": "0eea500455ef35cc8e4eb18a4b041541",
+    "url": "content/images/Spoon_favicon.svg"
+  },
+  {
+    "revision": "b23e519aae8b780cefcb9c74103b485b",
+    "url": "content/images/spoon-ui.png"
+  },
+  {
+    "revision": "6cf500559403ed8d5c9d4c59ed889132",
+    "url": "content/images/spoon-ui192.png"
+  },
+  {
+    "revision": "11a35b6eb658d35f2467e49235c4e70c",
+    "url": "content/images/chevron-right.svg"
+  },
+  {
+    "revision": "c2a4ba6ef374ac789bfb111957e2ae79",
+    "url": "content/css/loading.css"
+  },
+  {
+    "revision": "eaba5d155a40b6d7ac603cfdcb4eb5af",
+    "url": "content/images/spoon-ui.svg"
+  },
+  {
+    "url": "content/11a35b6eb658d35f2467e49235c4e70c.svg"
+  },
+  {
+    "url": "app/vendors.d14b65e1607afe4dd6c8.chunk.js"
+  },
+  {
+    "url": "app/main.d14b65e1607afe4dd6c8.bundle.js"
+  },
+  {
+    "revision": "23b54edea462e74c4ad5df9598ac6971",
+    "url": "./i18n/en.json"
+  },
+  {
+    "revision": "c12bf5176d9d1a469a2f0018d1510c0d",
+    "url": "./i18n/de.json"
+  }
+]);
---
+++ precache-manifest.62505a1563b9c1d0a290a42de8264e72.js
@@ -0,0 +1,88 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa6df0bd13c002481c98db1e202ec8a",
+    "url": "content/images/frame.svg"
+  },
+  {
+    "url": "content/main.bb63677ab4f315e4f14d.css"
+  },
+  {
+    "url": "content/vendors.bb63677ab4f315e4f14d.css"
+  },
+  {
+    "revision": "0819d5a799bf28d8dc90b39739327c34",
+    "url": "index.html"
+  },
+  {
+    "revision": "a67b8d690ca9b440a338ef9e34194b93",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "be66612e582d0f05047a7cf3e3ddc606",
+    "url": "manifest.webapp"
+  },
+  {
+    "revision": "d4a5d598e8510d03a187a3a2b8bda2e0",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "eaba5d155a40b6d7ac603cfdcb4eb5af",
+    "url": "content/images/spoon-ui.svg"
+  },
+  {
+    "revision": "72fd8c159e274e016b28ac1b46a34f02",
+    "url": "content/images/triangle.svg"
+  },
+  {
+    "revision": "d001004f765a00ec6cd7f89c782e506a",
+    "url": "content/images/spoon-ui256.png"
+  },
+  {
+    "url": "content/72fd8c159e274e016b28ac1b46a34f02.svg"
+  },
+  {
+    "revision": "b77cd177df9e44a1d3eb06e1cafce30d",
+    "url": "content/images/spoon-ui512.png"
+  },
+  {
+    "revision": "0eea500455ef35cc8e4eb18a4b041541",
+    "url": "content/images/Spoon_favicon.svg"
+  },
+  {
+    "revision": "b23e519aae8b780cefcb9c74103b485b",
+    "url": "content/images/spoon-ui.png"
+  },
+  {
+    "revision": "6cf500559403ed8d5c9d4c59ed889132",
+    "url": "content/images/spoon-ui192.png"
+  },
+  {
+    "revision": "11a35b6eb658d35f2467e49235c4e70c",
+    "url": "content/images/chevron-right.svg"
+  },
+  {
+    "revision": "c2a4ba6ef374ac789bfb111957e2ae79",
+    "url": "content/css/loading.css"
+  },
+  {
+    "revision": "b8bec99789f3b935c26c36ac3658ca18",
+    "url": "content/images/spoon-ui384.png"
+  },
+  {
+    "url": "content/11a35b6eb658d35f2467e49235c4e70c.svg"
+  },
+  {
+    "url": "app/vendors.bb63677ab4f315e4f14d.chunk.js"
+  },
+  {
+    "url": "app/main.bb63677ab4f315e4f14d.bundle.js"
+  },
+  {
+    "revision": "c1d7baf881fac4e6ec9d86b1c607f7cb",
+    "url": "./i18n/en.json"
+  },
+  {
+    "revision": "1a68f46eb8af24fc1b6b5c4025c25f30",
+    "url": "./i18n/de.json"
+  }
+]);
