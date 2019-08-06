8d3b42f5418e41645b859abf3df38035beb26b4e
Michael van Niekerk <mvniekerk@chazaqdev.com>
2019-08-06 10:39:24 +0200

    Date format validation format check

--- lib/validation/date.ts
+++ lib/validation/date.ts
@@ -1,8 +1,8 @@
 import moment from 'moment';
 import { Validate } from './validate';
 
-export const dateIsYyMmDd: Validate<string> = (i18nKey, val) =>
-  !!val && val.length >= 6 && moment(val.substr(0, 6), 'YYMMDD', true).format() !== 'Invalid date'
+export const createDateValidator: (format: string) => Validate<string> = format => (i18nKey, val) =>
+  !!val && val.length >= format.length && moment(val.substr(0, format.length), format, true).format() !== 'Invalid date'
     ? []
     : [
         {
@@ -11,3 +11,6 @@ export const dateIsYyMmDd: Validate<string> = (i18nKey, val) =>
           display: i18nKey
         }
       ];
+
+export const dateIsYyMmDd: Validate<string> = createDateValidator('YYMMDD');
+export const dateIsDdMmYyyy: Validate<string> = createDateValidator('DD/MM/YYYY');
--- test/javascript/spec/lib/date.spec.tsx
+++ test/javascript/spec/lib/date.spec.tsx
@@ -1,4 +1,4 @@
-import { dateIsYyMmDd } from '../../../../lib/validation/date';
+import { dateIsYyMmDd, dateIsDdMmYyyy } from '../../../../lib/validation/date';
 
 describe('YYMMDD formatting checks', () => {
     it('Correct dates should not return errors', () => {
@@ -34,6 +34,45 @@ describe('YYMMDD formatting checks', () => {
     it('Dates with wrong days should return errors', () => {
         ['221000', '8606-1', '870832', '000230'].forEach(val => {
             const errors = dateIsYyMmDd('Code coverage fudge', val);
+            expect(errors.length).toBeGreaterThan(0);
+        });
+    });
+});
+
+describe('DD/MM/YYYY formatting checks', () => {
+    it('Correct dates should not return errors', () => {
+        ['15/10/2022AAAAA', '06/06/1986', '13/07/1987DDDD', '02/01/2000000102EEE'].forEach(val => {
+            const errors = dateIsDdMmYyyy('', val);
+            expect(errors.length).toBe(0);
+        });
+    });
+    it('Too short numbers should return errors', () => {
+       ['22101', '8', '87'].forEach(val => {
+           const errors = dateIsDdMmYyyy('', val);
+           expect(errors.length).toBeGreaterThan(0);
+       });
+    });
+    it('Dates with non-numbers in should return errors', () => {
+       ['22.015', 'A60606', '8!@229', ' 23%332'].forEach(val => {
+           const errors = dateIsDdMmYyyy('', val);
+           expect(errors.length).toBeGreaterThan(0);
+       });
+    });
+    it('Null and undefined should return errors', () => {
+        [null, undefined].forEach(val => {
+            const errors = dateIsDdMmYyyy('', val);
+            expect(errors.length).toBeGreaterThan(0);
+        });
+    });
+    it('Dates with wrong months should return errors', () => {
+        ['15/13/2022', '06/00/86', '13/-1/87', '005002'].forEach(val => {
+            const errors = dateIsDdMmYyyy('', val);
+            expect(errors.length).toBeGreaterThan(0);
+        });
+    });
+    it('Dates with wrong days should return errors', () => {
+        ['00/10/2022', '-1/06/1986', '32/08/1987', '30/20/2000'].forEach(val => {
+            const errors = dateIsDdMmYyyy('Code coverage fudge', val);
             expect(errors.length).toBeGreaterThan(0);
         });
     });
