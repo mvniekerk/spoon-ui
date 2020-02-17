8c56d6ce8a4fdb494ae656275e9031a8675fe0a4
Anna Guzniak <rudenkoav@gmail.com>
2020-02-17 15:27:06 +0300

    fix lint

--- lib/components/button/ToggleButtonGroup.tsx
+++ lib/components/button/ToggleButtonGroup.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Button } from './button';
 import { Row } from 'reactstrap';
+import { ITranslatedValue, translateItem } from 'lib/util';
 
-export interface IToggleButtonGroupProps {
-  values: Array<string>;
+export interface IToggleButtonGroupProps<T> {
+  values: Array<ITranslatedValue<T>>;
 }
 
 export interface IToggleButtonGroupState {
   toggleId: number;
 }
 
-export class ToggleButtonGroup extends React.Component<IToggleButtonGroupProps, IToggleButtonGroupState> {
-  static defaultProps: IToggleButtonGroupProps = {
+export class ToggleButtonGroup<T> extends React.Component<IToggleButtonGroupProps<T>, IToggleButtonGroupState> {
+  static defaultProps: IToggleButtonGroupProps<any> = {
     values: []
   };
 
@@ -27,8 +28,9 @@ export class ToggleButtonGroup extends React.Component<IToggleButtonGroupProps, 
     return (
       <Row>
         {this.props.values.map((v, index) => (
+          // tslint:disable:jsx-no-lambda
           <Button className="toggle-btn" key={index} active={this.state.toggleId === index} onClick={() => this.onCheckboxBtnClick(index)}>
-            {v}
+            {translateItem(v)}
           </Button>
         ))}
       </Row>
